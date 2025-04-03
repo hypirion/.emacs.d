d695111b9bad70d21c7f046c6ad7ea3f0dc4b9ff
Jean Niklas L'orange <jeannikl@hypirion.com>
2025-04-03 18:14:54 +0200

    Let's have paredit globally

--- hypirion/hypirion-parens.el
+++ hypirion/hypirion-parens.el
@@ -1,8 +1,43 @@
-(use-package paredit
-  :bind (:map paredit-mode-map
-              ("M-n" . paredit-splice-sexp-killing-forward)
-              ("M-p" . paredit-splice-sexp-killing-backward))
-  :hook (emacs-lisp-mode . paredit-mode))
+(use-package smartparens
+  :ensure t
+  :hook ((emacs-lisp-mode . smartparens-strict-mode)
+         (lisp-mode        . smartparens-strict-mode)
+         (lisp-interaction-mode . smartparens-strict-mode)
+         (ielm-mode        . smartparens-strict-mode)
+         (clojure-mode     . smartparens-strict-mode)
+         (scheme-mode      . smartparens-strict-mode))
+  :bind (:map smartparens-mode-map
+              ("C-)" . sp-forward-slurp-sexp)
+              ("C-}" . sp-forward-barf-sexp)
+              ("C-(" . sp-backward-slurp-sexp)
+              ("C-{" . sp-backward-barf-sexp)
+              ("M-n" . sp-splice-sexp-killing-forward)
+              ("M-p" . sp-splice-sexp-killing-backward))
+  :config
+  (require 'smartparens-config)
+
+  ;; Enable globally, but strict mode in Lisp buffers
+  (smartparens-global-mode t)
+
+  (sp-use-paredit-bindings)
+
+  :custom
+  (sp-autoinsert-pair t)
+  (sp-ignore-modes-list nil) ;; apply smartparens everywhere
+
+  ;; Better backspace/delete behavior in strict mode
+  (sp-autodelete-pair t)
+  (sp-cancel-autoskip-on-backward-movement nil))
+
+;; Some FIXMEs:
+;;
+;; C-0 C-k doesn't delete backwards of the current line
+;;
+;; (|) -- sp-splice-sexp-killing-backward
+;; |)
+;; (wtf)
+;;
+;; Parens are also balanced inside comments. Hrm.
 
 (use-package highlight-parentheses
   :demand t
