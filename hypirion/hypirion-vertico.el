cdeec33a6dea79525a4991bf0a8d5dfbb2c0964b
Jean Niklas L'orange <jeannikl@hypirion.com>
2025-04-03 18:16:09 +0200

    Get back Ivy functionality with some elisp magic

--- hypirion/hypirion-vertico.el
+++ hypirion/hypirion-vertico.el
@@ -1,6 +1,36 @@
 (use-package vertico
   :init
-  (vertico-mode))
+  (vertico-mode)
+
+  ;; Custom functions for enhanced behavior
+  :config
+  (defun hypirion-delete-directory-with-backspace ()
+    "Delete entire directory name when pressing backspace at a path separator."
+    (interactive)
+    (if (and (eq (char-before) ?/)
+             (> (point) (minibuffer-prompt-end)))
+        (let ((end (point))
+              (start (save-excursion
+                       (backward-char)
+                       (while (and (> (point) (minibuffer-prompt-end))
+                                   (not (eq (char-before) ?/)))
+                         (backward-char))
+                       (point))))
+          (delete-region start end))
+      (call-interactively #'backward-delete-char)))
+
+  (defun hypirion-vertico-tab-action ()
+    "Open the selected file if it's the only candidate, otherwise do normal TAB."
+    (interactive)
+    (if (and (= (length vertico--candidates) 1)
+             (not (string-suffix-p "/" (car vertico--candidates))))
+        (vertico-exit)
+      (vertico-insert)))
+
+  ;; Add these key bindings to the vertico-map
+  :bind (:map vertico-map
+              ("DEL" . hypirion-delete-directory-with-backspace)
+              ("TAB" . hypirion-vertico-tab-action)))
 
 (use-package vertico-posframe
   :init
