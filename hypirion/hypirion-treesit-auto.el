9679e3233756e378f2f4012feb61f36afc87304e
Jean Niklas L'orange <jeannikl@hypirion.com>
2025-08-12 02:40:36 +0200

    Don't allow ts for incompatible impls
    If the full install didn't work, it certainly won't keep working without
    this package (?) being updated.

--- hypirion/hypirion-treesit-auto.el
+++ hypirion/hypirion-treesit-auto.el
@@ -1,6 +1,6 @@
 (use-package treesit-auto
   :custom
-  (treesit-auto-install 'prompt)
+  (treesit-auto-install nil)
   :config
   (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode)) ;; TODO this doesn't seem to work..?
