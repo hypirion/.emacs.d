b0f4b7d5398db19163bf87df11be5528140da9f6
Jean Niklas L'orange <jeannikl@hypirion.com>
2025-11-24 18:01:36 +0100

    Find working treesit versions
    These all have to be v14, not v15, which is Emacs doesn't support yet.

--- hypirion/hypirion-treesit-auto.el
+++ hypirion/hypirion-treesit-auto.el
@@ -11,6 +11,16 @@
    '((bash . ("https://github.com/tree-sitter/tree-sitter-bash" "v0.23.3" nil nil nil))
      (go . ("https://github.com/tree-sitter/tree-sitter-go" "v0.23.4" nil nil nil))
      (gomod . ("https://github.com/camdencheek/tree-sitter-go-mod" "v1.0.2" nil nil nil))
-     (python . ("https://github.com/tree-sitter/tree-sitter-python" "v0.23.6" nil nil nil)))))
+     (python . ("https://github.com/tree-sitter/tree-sitter-python" "v0.23.6" nil nil nil))
+     (css . ("https://github.com/tree-sitter/tree-sitter-css" "v0.23.2" nil nil nil))
+     (html . ("https://github.com/tree-sitter/tree-sitter-html" "v0.23.2" nil nil nil))
+     (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "v0.23.1" nil nil nil))
+     (json . ("https://github.com/tree-sitter/tree-sitter-json" "v0.24.8" nil nil nil))
+     (markdown . ("https://github.com/tree-sitter-grammars/tree-sitter-markdown" "v0.4.1" nil nil nil))
+     (rust . ("https://github.com/tree-sitter/tree-sitter-rust" "v0.23.3" nil nil nil))
+     ))
+  (treesit-auto-langs '(bash go gomod python css html javascript json markdown kdl))
+  ;; Note: rust excluded - using rust-mode with treesitter-derive instead
+  )
 
 (provide 'hypirion-treesit-auto)
