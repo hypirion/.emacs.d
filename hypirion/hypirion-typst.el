7f8895ba61e1000744c8436f146573eff8ca68a4
Jean Niklas L'orange <jeannikl@hypirion.com>
2025-11-29 13:31:36 +0100

    Add comment on how to get typst mode working

--- hypirion/hypirion-typst.el
+++ hypirion/hypirion-typst.el
@@ -1,4 +1,6 @@
 (use-package typst-ts-mode
   :ensure (:type git :host codeberg :repo "meow_king/typst-ts-mode"))
 
+;; Note: do typst-ts-mc-install-grammar to install the grammar
+
 (provide 'hypirion-typst)
